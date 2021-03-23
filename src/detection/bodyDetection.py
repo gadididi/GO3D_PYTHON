d643e0954cbbf81452493685c18ea1978d3887a7
Gadiididi <gadi.didi85@gmail.com>
2021-03-23 09:57:16 +0200

    added new vr of detection

--- src/detection/bodyDetection.py
+++ src/detection/bodyDetection.py
@@ -1,5 +1,4 @@
 import cv2
-import time
 import numpy as np
 import matplotlib
 from matplotlib import pyplot as plt
@@ -8,40 +7,35 @@ from tensorflow.keras.models import load_model
 matplotlib.use("TkAgg")
 
 
-def realtime_demo(img_wh):
-    model_path = './models/enet256_weight0501.hdf5'
-    model = load_model(model_path)
-    cap = cv2.VideoCapture(0)
+class HumanPartSegmentationDetector:
+    def __init__(self, model_path):
+        self.__model = load_model(model_path)
+        self.__size_of_img = 256
+        self.__num_of_class = 7
 
-    if "64" in model_path:
-        img_dec_wh = 64
-    elif "256" in model_path:
-        img_dec_wh = 256
-    num_classes = 7
+    # TODO: change path to ndarry in the future
+    def detect(self, path_o_img):
+        orig_img = cv2.imread(path_o_img, cv2.IMREAD_UNCHANGED)
 
-    predict_times = []
-
-    while True:
-        # Capture frame-by-frame
-        # ret, orig_img = cap.read()
-        orig_img = cv2.imread("./test.jpg", cv2.IMREAD_UNCHANGED)
-        bgr_img = cv2.resize(orig_img, (img_wh, img_wh))
+        # scale the img to 256*256*3 input for neural network
+        bgr_img = cv2.resize(orig_img, (256, 256))
         img = bgr_img[..., ::-1]
         img = img * (1 / 255.0)
 
         # Add batch dimension: 1 x D x D x 3
         img_tensor = np.expand_dims(img, 0)
 
-        raw_output = model.predict(img_tensor)
-        output = np.reshape(raw_output, (1, img_dec_wh, img_dec_wh, num_classes))
+        raw_output = self.__model.predict(img_tensor)
+        output = np.reshape(raw_output, (1, self.__size_of_img, self.__size_of_img, self.__num_of_class))
 
         # Get mask
         seg_labels = output[0, :, :, :]
         seg_img = np.argmax(seg_labels, axis=2)
 
         # Display
-        # seg_img = seg_img[:, :, np.newaxis]
-        # seg_img = cv2.resize(seg_img, (640, 480))
+        seg_img = seg_img[:, :, np.newaxis]
+        seg_img = np.array(seg_img, dtype='uint8')
+        seg_img = cv2.resize(seg_img, (640, 480))
         display_img = cv2.resize(bgr_img, (640, 480))
         plt.figure(1)
         plt.imshow(seg_img)
@@ -51,12 +45,11 @@ def realtime_demo(img_wh):
         cv2.imshow('img', display_img)
         cv2.waitKey(0)
         cv2.destroyAllWindows()
-        # if cv2.waitKey(1) & 0xFF == ord('q'):
-        #     break
+        self.find_body_part(seg_img)
 
-        # When everything done, release the capture
-        cap.release()
-        cv2.destroyAllWindows()
+    def find_body_part(self, image):
+        ...
 
 
-realtime_demo(256)
+d = HumanPartSegmentationDetector("./models/enet256_weight0501.hdf5")
+d.detect('./man2.jpg')
