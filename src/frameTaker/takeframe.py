0ab87ee65661785fe780a58aee8a2b0095cd1047
Gadiididi <gadi.didi85@gmail.com>
2021-04-26 18:34:18 +0300

    fixed: bug in is_healthy method in takeframe.py

--- src/frameTaker/takeframe.py
+++ src/frameTaker/takeframe.py
@@ -33,7 +33,7 @@ class FrameTaker:
         self._exit_scan = False
         self._last_image = None
         self._last_depth_image = None
-        self._healthy = True
+        self._healthy = False
 
     def start_stream(self):
         # Start streaming
@@ -72,9 +72,11 @@ class FrameTaker:
 
                 cv2.namedWindow('RealSense', cv2.WINDOW_AUTOSIZE)
                 depth_map_show = None
-                depth_map_show = cv2.normalize(depth_image, depth_map_show, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_8U)
+                depth_map_show = cv2.normalize(depth_image, depth_map_show, alpha=0, beta=255,
+                                               norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_8U)
                 depth_map_show = cv2.applyColorMap(depth_map_show, cv2.COLORMAP_COOL)
-                cv2.circle(depth_map_show, (int(depth_map_show.shape[1] / 2), int(depth_map_show.shape[0] / 2)), 2, (0, 0, 255),
+                cv2.circle(depth_map_show, (int(depth_map_show.shape[1] / 2), int(depth_map_show.shape[0] / 2)), 2,
+                           (0, 0, 255),
                            -1)
                 self._last_image = depth_map_show
                 self._last_depth_image = depth_image
@@ -112,4 +114,4 @@ class FrameTaker:
         return self._last_depth_image
 
     def is_healthy(self):
-        return self.is_healthy()
+        return self._healthy
